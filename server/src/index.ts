2f0fc53b8f6c4cd139b3057782e5c368207c0dd1
nindob <nindob@users.noreply.github.com>
2025-05-06 10:59:20 -0400

    prototype

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,29 +2,36 @@ import type { ServerWebSocket } from "bun";
 import { Hono } from "hono";
 import { createBunWebSocket } from "hono/bun";
 
-const { upgradeWebSocket, websocketHandler } = createBunWebSocket<ServerWebSocket>();
+const { upgradeWebSocket } = createBunWebSocket<ServerWebSocket>();
 
 const app = new Hono();
 
-app.get("/", (c) => { return c.text("Hannah is a twig!");});
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
 
 app.get(
-    "/ws",
-    upgradeWebSocket((c) => {
-        return {
-            onMessage(event, ws) {
-                console.log(`Message from client: ${event.data}`);
-                ws.send("Hello from server!");
-            },
-            onClose: () => {
-                console.log("Connection closed");
-            },
-        };
-    })
+  "/ws",
+  upgradeWebSocket((c) => {
+    return {
+      onMessage(event, ws) {
+        console.log(`Message from client: ${event.data}`);
+        ws.send(event.data.toString()); // send the same message back to the client
+      },
+      onClose: () => {
+        console.log("Connection closed");
+      },
+      onOpen: () => {
+        console.log(`Connection opened from`);
+      },
+      onError: (error) => {
+        console.error("Error", error);
+      },
+    };
+  })
 );
 
 export default {
-    port: 8080,
-    fetch: app.fetch,
-    websocket,
+  port: 8080,
+  fetch: app.fetch,
 };
