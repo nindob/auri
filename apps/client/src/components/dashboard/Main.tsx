e3788e098f3c6ca806339f2c02794c9bf628720c
nindob <nindob@users.noreply.github.com>
2025-05-22 13:23:11 -0400

    apply scroll styles

--- apps/client/src/components/dashboard/Main.tsx
+++ apps/client/src/components/dashboard/Main.tsx
@@ -1,11 +1,17 @@
+import { cn } from "@/lib/utils";
 import { motion } from "framer-motion";
 import { Queue } from "../Queue";
 
 export const Main = () => {
   return (
-    <motion.div className="w-full lg:flex-1 overflow-y-auto bg-gradient-to-b from-neutral-900/50 to-neutral-950 backdrop-blur-xl">
+    <motion.div
+      className={cn(
+        "w-full lg:flex-1 overflow-y-auto bg-gradient-to-b from-neutral-900/90 to-neutral-950 backdrop-blur-xl bg-neutral-950 h-full",
+        "scrollbar-thin scrollbar-thumb-rounded-md scrollbar-thumb-muted-foreground/10 scrollbar-track-transparent hover:scrollbar-thumb-muted-foreground/20"
+      )}
+    >
       <motion.div className="p-6 pt-4">
-        {/* <h1 className="text-xl font-semibold mb-8">Auri</h1> */}
+        {/* <h1 className="text-xl font-semibold mb-8">BeatSync</h1> */}
         <Queue className="mb-8" />
       </motion.div>
     </motion.div>
