22cccc82c1717049061b0ec1221052d75baabefa
nindob <nindob@users.noreply.github.com>
2025-05-23 15:17:18 -0400

    get rid of old search music button

--- apps/client/src/components/dashboard/Left.tsx
+++ apps/client/src/components/dashboard/Left.tsx
@@ -60,15 +60,7 @@ export const Left = ({ className }: LeftProps) => {
           <span>Default Library</span>
         </Button>
 
-        <a href="https://cobalt.tools/" target="_blank">
-          <Button
-            className="w-full flex justify-start gap-3 py-2 text-white font-medium bg-white/10 hover:bg-white/15 rounded-md text-xs transition-colors duration-200 cursor-pointer"
-            variant="ghost"
-          >
-            <Search className="h-4 w-4" />
-            <span>Search Music</span>
-          </Button>
-        </a>
+        <SearchMusic />
       </motion.div>
 
       <Separator className="bg-neutral-800/50" />
@@ -96,7 +88,6 @@ export const Left = ({ className }: LeftProps) => {
 
         <div className="pl-1 flex flex-col space-y-2">
           <AudioUploaderMinimal />
-          <SearchMusic />
         </div>
       </motion.div>
     </motion.div>
